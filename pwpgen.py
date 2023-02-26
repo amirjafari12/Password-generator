606aa78f388c8167dfaa1166d4f480bb5babef46
amirjafari12 <120090195+amirjafari12@users.noreply.github.com>
2023-02-26 21:32:12 +0100

    Update pwpgen.py

--- pwpgen.py
+++ pwpgen.py
@@ -1,19 +1,23 @@
+import string
 import random
 
-groß_buchstaben = ["A","B","C","D","E","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-klein_buchstaben = ["a","b","c","d","e","f","g","h","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-zahlen = range(0,9)
-zeichen = ["!","?","§","%"]
+letter = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
 
-A = input("Möchten sie ein Passwort generieren [y/n]?: ")
+A = letter + digits + special_chars
 
+decision = input("Möchten sie ein Passwort generieren [y/n]?: ")
 
 def x():
-    if A == "y":
-      print(random.choice(groß_buchstaben),random.choice(klein_buchstaben),random.choice(zahlen),random.choice(zeichen), random.choice(groß_buchstaben),random.choice(klein_buchstaben),random.choice(zahlen),random.choice(zeichen))
+    pwd = ""
+    if decision == "y":
+        B = int(input("Anzahl der Elemente: "))
+        for i in range(B):
+            pwd += "".join(random.choice(A))
+        print(pwd)
 
     else:
         print("Sie haben sich für nein entschieden")
-
 
 x()
